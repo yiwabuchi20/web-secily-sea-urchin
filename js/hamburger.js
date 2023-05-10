e81f07356b7496316bd0d10b5a2bcc57023cc8cd
yiwabuchi <yiwabuchi20@gmail.com>
2023-05-10 14:31:18 +0900

    add: ハンバーガーメニューを作成した

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
-    $("#nav-button").on("click",function(){
-      $(".sp-nav").slideToggle();
-    });
+$(document).ready(function () {
+  $("#nav-button").on("click", function () {
+    var menu = $(".sp-nav"), // スライドインするメニューを指定
+      menuBtn = $("#nav-button"), // メニューボタンを指定
+      menuWidth = menu.outerWidth();
+
+      menuBtn.toggleClass("open");
+    if (menuBtn.hasClass("open")) {
+      menu.stop().animate({ left: 0 }, 300);
+      $(".fa-times").css("display","block");
+      $(".fa-bars").css("display","none");
+    } else {
+      menu.stop().animate({ left: +menuWidth }, 300);
+      $(".fa-times").css("display","none");
+      $(".fa-bars").css("display","block");
+    }
   });
+});
