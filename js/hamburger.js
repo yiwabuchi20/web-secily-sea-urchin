cdab279de9140410be4837397da905d796175e88
yiwabuchi <yiwabuchi20@gmail.com>
2023-05-10 15:21:12 +0900

    fix: メニューのアニメーションを調整した

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,15 +4,18 @@ $(document).ready(function () {
       menuBtn = $("#nav-button"), // メニューボタンを指定
       menuWidth = menu.outerWidth();
 
-      menuBtn.toggleClass("open");
+    menuBtn.toggleClass("open");
     if (menuBtn.hasClass("open")) {
+      $(".sp-nav").css("display", "block");
       menu.stop().animate({ left: 0 }, 300);
-      $(".fa-times").css("display","block");
-      $(".fa-bars").css("display","none");
+      $(".fa-times").css("display", "block");
+      $(".fa-bars").css("display", "none");
     } else {
-      menu.stop().animate({ left: +menuWidth }, 300);
-      $(".fa-times").css("display","none");
-      $(".fa-bars").css("display","block");
+      menu.stop().animate({ left: +menuWidth }, 300, function () {
+        $(".sp-nav").css("display", "none");
+      });
+      $(".fa-times").css("display", "none");
+      $(".fa-bars").css("display", "block");
     }
   });
 });
